a69ca03f3f890f3e0aa265300abfae7d0267dd2c
Deivid R. S. <deivid@schiitz.com>
2024-12-07 18:24:25 -0400

    remove

--- frontend_web/src/context/userIdContext.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const userIdContext = React.createContext("");
-
-export default userIdContext;
--- frontend_web/src/services/api.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({ baseURL: "localhost:2999" });
-
-export default api;
